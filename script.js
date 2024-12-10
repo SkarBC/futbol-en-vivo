3c311c95fcec91c4dfa2cf1df547daaf2687f238
SkarBC <sadrackbarrios2004@gmail.com>
2024-12-10 04:35:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("¡Bienvenido a Fútbol en Vivo!");
+const apiKey = '8e27fefad48244b0b6180620d150c2ad';
+const url = `https://api-football-v1.p.rapidapi.com/v3/fixtures?live=true`;
+
+fetch(url, {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': apiKey,
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    const resultadosDiv = document.getElementById('resultados-en-vivo');
+    let resultadosHTML = '<h3>Partidos en vivo</h3>';
+    data.response.forEach(partido => {
+        resultadosHTML += `
+            <p>${partido.teams.home.name} vs ${partido.teams.away.name} - ${partido.goals.home} : ${partido.goals.away}</p>
+        `;
+    });
+    resultadosDiv.innerHTML = resultadosHTML;
+})
+.catch(err => {
+    console.error('Error:', err);
+});
